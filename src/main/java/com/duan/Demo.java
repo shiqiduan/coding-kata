49d596986f4e4898ffd19068daed04dc7657b756
DerekDuan <duan.shiqi@gmail.com>
2023-01-16 17:59:56 +0800

    leetcode

---
+++ src/main/java/com/duan/Demo.java
@@ -0,0 +1,5 @@
+package com.duan;
+
+public class Demo {
+
+}
---
+++ src/main/java/com/duan/leetcode/E74.java
@@ -0,0 +1,52 @@
+package com.duan.leetcode;
+
+public class E74 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int small = matrix[0][0];
+        int large = matrix[matrix.length - 1][matrix[0].length - 1];
+        if (target < small || target > large) {
+            return false;
+        }
+
+        int line = 0;
+        int start = 0, end = matrix.length - 1;
+        if (target >= matrix[end][0]) {
+            line = end;
+        } else {
+            while (start < end) {
+                int mid = start + (end - start + 1) / 2;
+                int midValue = matrix[mid][0];
+
+                if (target >= midValue) {
+                    start = mid;
+                } else {
+                    end = mid - 1;
+                }
+            }
+            line = start;
+        }
+
+        start = 0;
+        end = matrix[0].length - 1;
+        while (start <= end) {
+            int mid = start + (end - start + 1) / 2;
+            int midValue = matrix[line][mid];
+
+            if (midValue == target) {
+                return true;
+            }
+            if (target > midValue) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        E74 e = new E74();
+
+        System.out.println(e.searchMatrix());
+    }
+}
