6f1e82f8958415a1ef7424f0323ea88a770934ec
DerekDuan <duan.shiqi@gmail.com>
2023-05-09 11:46:22 +0800

    leetcode

---
+++ leetcode/src/main/java/com/coding/E9.java
@@ -0,0 +1,14 @@
+package com.coding;
+
+public class E9 {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String a = x + "";
+        int l = 0, r = a.length() - 1;
+
+        while (l < r) {
+            if (a.charAt(l++) != a.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
