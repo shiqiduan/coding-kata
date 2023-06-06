583456614be6f8fb72baf78a21ab970f35f886a3
DerekDuan <duan.shiqi@gmail.com>
2023-06-06 18:57:01 +0800

    netty demo

--- netty-demo/src/main/java/io/netty/example/echo/EchoServer.java
+++ netty-demo/src/main/java/io/netty/example/echo/EchoServer.java
@@ -1,6 +1,8 @@
 package io.netty.example.echo;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
@@ -8,14 +10,17 @@ import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.handler.logging.LogLevel;
 import io.netty.handler.logging.LoggingHandler;
 
+import java.nio.charset.Charset;
+
 public class EchoServer {
     private static int port = 8080;
 
     public static void main(String[] args) throws InterruptedException {
-        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup bossGroup = new NioEventLoopGroup(2);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
         EchoServerHandler serverHandler = new EchoServerHandler();
+        EchoServerHandler2 serverHandler2 = new EchoServerHandler2();
 
         try {
             ServerBootstrap b = new ServerBootstrap();
@@ -27,8 +32,8 @@ public class EchoServer {
                         @Override
                         protected void initChannel(SocketChannel ch) throws Exception {
                             ChannelPipeline p = ch.pipeline();
-
                             p.addLast(serverHandler);
+                            p.addLast(serverHandler2);
                         }
                     });
 
@@ -45,12 +50,45 @@ public class EchoServer {
 class EchoServerHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        ctx.write(msg);
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString());
+        // ctx.write(in);
+        ctx.fireChannelRead(msg);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        ctx.flush();
+        System.out.println("Server - channelReadComplete: ");
+        ctx.fireChannelReadComplete();
+        // ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
+
+/**
+ * 第一个问题：如何串联多个 handler ？
+ */
+@ChannelHandler.Sharable
+class EchoServerHandler2 extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server 2 received: " + in.toString());
+        
+        ByteBuf buf =
+                Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hello\r\n", Charset.forName("UTF-8")));
+        ctx.writeAndFlush(buf);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("Server 2 - channelReadComplete: ");
+        // ctx.flush();
     }
 
     @Override
